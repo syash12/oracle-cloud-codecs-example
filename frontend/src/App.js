3081c6939d1d934b5553318fcaa57eaae66f58e9
Yash Shahi <syash12@gmail.com>
2023-03-30 20:03:41 -0500

    can start recording but stop doesn't work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,62 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const [audioChunks, setAudioChunks] = useState([]);
+
+  useEffect(() => {
+    let recorder;
+    if (isRecording) {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+          recorder = new MediaRecorder(stream);
+          setMediaRecorder(recorder);
+          recorder.start();
+          recorder.addEventListener('dataavailable', event => {
+            setAudioChunks(audioChunks => [...audioChunks, event.data]);
+          });
+        })
+        .catch(error => console.error(error));
+    }
+  
+    // add stop event listener outside the else block
+    if (mediaRecorder !== null) {
+      mediaRecorder.addEventListener('stop', () => {
+        if (mediaRecorder.state !== 'inactive') {
+          mediaRecorder.stop();
+          setMediaRecorder(null);
+          const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg-3' });
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          audio.play();
+        }
+      });
+    }
+  }, [isRecording, mediaRecorder, audioChunks]);
 
   const handlePlayClick = () => {
-    setIsPlaying(!isPlaying);
+    setIsRecording(true);
   };
+
+  const handleStopClick = () => {
+    setIsRecording(false);
+  };
+
 
   return (
     <div className="App">
       <header className="App-header">
         <span style={{fontSize:"4em"}} onClick={handlePlayClick}> 
-            {isPlaying ? "■" : "▶"} 
+            {isRecording ? null : "▶"} 
         </span>
+          {isRecording ? (
+              <span style={{fontSize:"4em"}} onClick={handleStopClick}> 
+                  ■
+              </span>
+          ) : null}
         <p> Hit play to start recording audio</p>
         <a className="App-link"
           href="https://github.com/syash12/oracle-cloud-codecs-example"
