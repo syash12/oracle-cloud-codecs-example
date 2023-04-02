7af2fe4aa32aed7a903e93938e3a26ef747b73d0
Yash Shahi <syash12@gmail.com>
2023-04-02 12:16:37 -0500

    updates to frontend and backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,15 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import os
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/transcribe")
-async def transcribe():
-    return {"message": "ahhhh im transcribiiiiing"}
+async def transcribe(request: Request):
+    # get the uploaded file
+    form = await request.form()
+    audio = form["audio"].file
+
+    # save the file to disk
+    filename = form["audio"].filename
+    file_path = os.path.join(filename)  # replace with the path where you want to save the file
+    #with open(file_path, "wb") as f:
+        #f.write(audio.read())
+    print("ahhhh im writing to disk")
+
+    return {"message": "audio file saved successfully"}
 
 
 if __name__ == '__main__':
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,12 @@ function App() {
   const [isRecording, setIsRecording] = useState(false);
   const [mediaRecorder, setMediaRecorder] = useState(null);
   const [audioChunks, setAudioChunks] = useState([]);
+  let recorder;
 
   useEffect(() => {
-    let recorder;
-    if (isRecording) {
-      navigator.mediaDevices.getUserMedia({ audio: true })
+    if (isRecording && mediaRecorder === null) {
+      console.log("inside play button hit")
+      navigator.mediaDevices.getUserMedia({ "audio": true, "video": false })
         .then(stream => {
           recorder = new MediaRecorder(stream);
           setMediaRecorder(recorder);
@@ -23,28 +24,53 @@ function App() {
     }
   
     // add stop event listener outside the else block
-    if (mediaRecorder !== null) {
+    if (mediaRecorder !== null && !isRecording) {
+      console.log("inside stop button hit")
       mediaRecorder.addEventListener('stop', () => {
-        if (mediaRecorder.state !== 'inactive') {
-          mediaRecorder.stop();
-          setMediaRecorder(null);
-          const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg-3' });
-          const audioUrl = URL.createObjectURL(audioBlob);
-          const audio = new Audio(audioUrl);
-          audio.play();
-        }
+        const audioCtx = new AudioContext();
+        const audio = new Audio();
+        audio.setAttribute("controls", "");
+        document.querySelector("#sound-clip").appendChild(audio);
+        document.querySelector("#sound-clip").insertAdjacentHTML('beforeend', '<br />');
+
+        const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg-3' });
+        audio.src = URL.createObjectURL(audioBlob);
+        audio.play();
+
+        // send audio to backend
+        const formData = new FormData();
+        formData.append('audio', audioBlob, 'audio.mp3');
+
+        // send audio data to service
+        fetch('http://localhost:8000/transcribe', {
+          method: 'POST',
+          body: formData
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to send audio data');
+          }
+          console.log('Audio data sent successfully');
+        })
+        .catch(error => console.error(error));
+
+        setMediaRecorder(null);
+        setAudioChunks([]);
+        setIsRecording(false);
       });
+      mediaRecorder.stop();
     }
-  }, [isRecording, mediaRecorder, audioChunks]);
+  }, [isRecording, mediaRecorder]);
 
   const handlePlayClick = () => {
+    console.log("play button hit")
     setIsRecording(true);
   };
 
   const handleStopClick = () => {
+    console.log("stop button hit");
     setIsRecording(false);
   };
-
 
   return (
     <div className="App">
@@ -52,12 +78,11 @@ function App() {
         <span style={{fontSize:"4em"}} onClick={handlePlayClick}> 
             {isRecording ? null : "▶"} 
         </span>
-          {isRecording ? (
-              <span style={{fontSize:"4em"}} onClick={handleStopClick}> 
-                  ■
-              </span>
-          ) : null}
+        <span style={{fontSize:"4em"}} onClick={handleStopClick}> 
+            {isRecording ? "■" : null} 
+        </span>
         <p> Hit play to start recording audio</p>
+        <div id="sound-clip"></div>
         <a className="App-link"
           href="https://github.com/syash12/oracle-cloud-codecs-example"
           target="_blank"
